7d20070e0bbf9eeee3d8ae6db3fc143c53969f12
Damian Yerrick <git@pineight.com>
2023-08-02 18:57:01 -0400

    hotbssvars: add ref, type, mod, and importcount
    and add a bunch of documentation for debug files

--- tools/hotbssvars.py
+++ tools/hotbssvars.py
@@ -21,15 +21,67 @@ import sys, os, argparse
 from collections import defaultdict, namedtuple
 import textwrap
 
-helpText="Finds frequently accessed BSS variables in an ld65 debug symbol file."
-helpEnd= ""
-
-def parse_argv(argv):
-    p = argparse.ArgumentParser(description=helpText, epilog=helpEnd)
-    p.add_argument("dbgfile", help="path of debug symbol file")
-    p.add_argument("-o", "--output", default='-',
-                   help="path of debug symbol file to write")
-    return p.parse_args(argv[1:])
+# ld65 debug file parsing, from hotbssvars.py #######################
+#
+# Keys in a "file" entry
+# "id": used by lines to refer to this file
+# "name": name of file in quotation marks
+#
+# Keys in a "line" entry
+# "id": used by ??? to refer to this line of code
+# "file": ID of file containing this line of code
+# "line": line number
+# "mtime": modification date as a UNIX timestamp
+# "mod": +-separated list of translation units containing this file
+# "span": +-separated list of spans that contain this line if any
+#     (may be multiple if macros are involved)
+#
+# Keys in a "seg" entry
+# "id": used by syms to refer to this segment
+# "name": name of segment in quotation marks
+# "oname": ROM file to which segment was written
+# "ooffs": starting address in the ROM file if any
+# "start": starting address in address space
+# "size": size in bytes
+# "type": "ro" for ROM, "rw" for RAM
+#
+# Keys in a "span" entry (things concatenated to make a segment;
+#     don't know if they can overlap)
+# "id": used by ??? to refer to this span
+# "seg": ID of the section containing it
+# "start": offset in some sense (from start of segment?)
+# "size": size in bytes
+#
+# Keys in a "scope" entry
+# "id":
+# "mod": translation unit defining this scope
+# "name": scope name in quotation marks, or "" for a TU's top level
+# "type": scope, struct, or (absent)
+# "parent": ID of enclosing scope if not top level
+# "sym": ID of symbol if this is a .proc
+# "span": spans related to this scope (don't know how .pushseg is treated)
+# "size": ?
+#
+# Keys in a "sym" entry
+# "id": used by other things to refer to this symbol
+# "name": name of symbol in quotation marks
+# "val": value of symbol
+# "addrsize": size of address (zeropage, absolute, far)
+# "seg": associated segment
+# "type": "equ" for an equate, "lab" for a label, or "imp" for
+#     something from another TU accessed via .import or .importzp
+# "exp": If type=imp, the ID of the sym being imported
+#     (a type=imp with no EXP is the unused __SIZE__ label of
+#     a segment with define=yes)
+# "scope": ID of a scope
+# "def": ID of lines where defined (if used in a macro, this is
+#     a +-separated list)
+# "ref": +-separated list of lines where used (may be duplicates if
+#     used in .rept)
+# "size": size in bytes
+#
+# Keys in a "type" entry
+# id and val, where val appears to be some hex bitfield
 
 def parse_nvp_line(line):
     line = line.rstrip().split("\t", 1)
@@ -39,13 +91,9 @@ def parse_nvp_line(line):
              else {})
     return symtype, props
 
-def fmt_ref(line, symfile):
-    # given a (file id, line number
-    fileid, linenum = linenums[line]
-    return "%s line %d in %s" % (filenames[fileid], linenum)
-
 SymFile = namedtuple("DefDicts", [
-    'filenames', 'linenums', 'syms', 'sym_refs', 'span_segs', 'segs', 'scopes'
+    'filenames', 'linenums', 'syms', 'sym_refs', 'span_segs', 'segs',
+    'scopes', 'modules', 'importcount'
 ])
 FileLine = namedtuple("FileLine", [
     'fileid', 'linenum', 'spans'
@@ -54,10 +102,13 @@ Segment = namedtuple("Segment", [
     "name", "start", "size", "ooffs"
 ])
 Sym = namedtuple("Sym", [
-    "name", "val", "scope", 'seg', 'def_lines'
+    "name", "val", "scope", 'seg', 'def_lines', 'ref_lines', 'size', 'type'
 ])
 Scope = namedtuple("Scope", [
-    "name", "parent", "spans"
+    "name", "parent", "spans", "mod"
+])
+TranslationUnit = namedtuple("TranslationUnit", [
+    "name", "fileid"
 ])
 
 def parse_symfile(lines):
@@ -71,6 +122,8 @@ def parse_symfile(lines):
     span_segs = {}  # spanid to segid
     segs = {}  # segid to Segment
     scopes = {}
+    mods = {}
+    importcount = defaultdict(int)
 
     for line in lines:
         symtype, props = parse_nvp_line(line)
@@ -93,7 +146,9 @@ def parse_symfile(lines):
             span_segs[int(props['id'])] = int(props['seg'])
         elif symtype == 'sym':
             name = props['name'].strip('"')
-            if props['type'] == 'imp':
+            symtype = props['type']
+            if symtype == 'imp':
+                importcount[name] += 1
                 try:
                     symid = int(props['exp'])
                 except KeyError:
@@ -103,9 +158,13 @@ def parse_symfile(lines):
                 value = int(props["val"], base=0) if "val" in props else None
                 scope = int(props['scope']) if 'scope' in props else None
                 seg = int(props['seg']) if 'seg' in props else None
+                size = int(props['size']) if 'size' in props else None
                 def_lines = [int(x) for x in props["def"].split("+")]
                 # Multiple def lines occur when macro uses .local or .scope
-                syms[symid] = Sym(name, value, scope, seg, def_lines)
+                ref_lines = props.get('ref')
+                ref_lines = [int(x) for x in ref_lines.split("+")] if ref_lines else ()
+                syms[symid] = Sym(name, value, scope, seg,
+                                  def_lines, ref_lines, size, symtype)
             refs = props.get('ref', '')
             if refs: sym_refs[symid].update(int(x) for x in refs.split("+"))
         elif symtype == 'scope':
@@ -113,11 +172,34 @@ def parse_symfile(lines):
             scopeid = int(props['id'])
             name = props['name'].strip('"')
             parent = int(props['parent']) if 'parent' in props else None
-            if 'span' not in props: print(props)
-            spans = [int(x) for x in props['span'].split("+")]
-            scopes[scopeid] = Scope(name, parent, spans)
+            spans = props.get('span', '')
+            # empty spans means a scope containing no bytes
+            spans = [int(x) for x in spans.split('+')] if spans else ()
+            mod = int(props['mod'])
+            scopes[scopeid] = Scope(name, parent, spans, mod)
+        elif symtype == 'mod':  # a translation unit
+            modid = int(props['id'])
+            name = props['name'].strip('"')
+            toplevelfile = int(props['file'])
+            mods[modid] = TranslationUnit(name, toplevelfile)
 
-    return SymFile(filenames, linenums, syms, sym_refs, span_segs, segs, scopes)
+    return SymFile(filenames, linenums, syms, sym_refs, span_segs, segs,
+                   scopes, mods, importcount)
+
+helpText="Finds frequently accessed BSS variables in an ld65 debug symbol file."
+helpEnd= ""
+
+def parse_argv(argv):
+    p = argparse.ArgumentParser(description=helpText, epilog=helpEnd)
+    p.add_argument("dbgfile", help="path of debug symbol file")
+    p.add_argument("-o", "--output", default='-',
+                   help="path of debug symbol file to write")
+    return p.parse_args(argv[1:])
+
+def fmt_ref(line, symfile):
+    # given a (file id, line number
+    fileid, linenum = linenums[line]
+    return "%s line %d in %s" % (filenames[fileid], linenum)
 
 def main(argv=None):
     args = parse_argv(argv or sys.argv)
